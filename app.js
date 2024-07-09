2f1a467bc938c341cfb3cd7602fe97811b6bec51
Selleck Elliott <selleck.elliott@gmail.com>
2024-07-09 08:58:49 -0600

    javascript edits

--- app.js
+++ app.js
@@ -6,5 +6,25 @@ console.log(
 // Exercise 1
 
 // Exercise 2
-
+const firstName = "Selleck";
+let lastName = "Elliott";
+var age = 36;
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+// Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+//Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+console.log(firstName, lastName, age)
